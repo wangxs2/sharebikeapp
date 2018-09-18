69163a1203a343eb43d5f30ddff2ef35170ebf39
LiYuewei0606 <13788916461@163.com>
2018-09-18 17:14:25 +0800

    修改密码+意见反馈

--- src/views/problemFeedback/index.vue
+++ src/views/problemFeedback/index.vue
@@ -9,20 +9,20 @@
     <div class="content">
       <div class="feedback-input-box">
         <div class="input-text-box">
-          <textarea class="input-text" placeholder="欢迎您向我们提出宝贵建议和意见" v-text="problemContent"></textarea>
+          <textarea class="input-text" placeholder="欢迎您向我们提出宝贵建议和意见" v-model="problemContent"></textarea>
         </div>
         <div class="input-img-box">
-          <div class="img-added-box">
-            <img class="img-added" src="" alt="">
+          <div class="img-added-box" v-for="(item,index) in showPhoto" :key="index">
+            <img class="img-added" :src="Ip + item" alt="">
             <img class="del-img" src="@/assets/image/problemFeedback/delete@2x.png" alt="" @click="deleteImg(index)">
           </div>
-          <img class="img-add-btn" src="@/assets/image/problemFeedback/add@2x.png" alt="" v-if="photo.length<4">
+          <img class="img-add-btn" src="@/assets/image/problemFeedback/add@2x.png" alt="" @click="addImg" v-if="photo.length<4">
           <div class="img-add-note" v-if="photo.length<4">至多上传四张图片</div>
         </div>
       </div>
       <div class="contact-box">
         <div class="info-title">联系方式</div>
-        <input class="info-input" type="text" placeholder="请输入手机号/邮箱，便于联系您" v-model="phoneNumber">
+        <input class="info-input" type="text" placeholder="请输入手机号/邮箱，便于联系您" v-model="phoneNumber" @blur="checkPhoneNumber">
       </div>
 
       <div class="bottom-btn">
@@ -36,18 +36,23 @@
 <script>
 import { mapGetters } from "vuex";
 import { MessageBox } from "mint-ui";
+import { validateContact } from "@/libs/validate.js";
 export default {
   computed: {},
   data() {
     return {
       problemContent: "",
       phoneNumber: "",
-      photo: []
+      photo: [],
+      showPhoto: [],
+      phoneNumberCheck: true
     };
   },
   components: {},
   mounted() {},
-  created() {},
+  created() {
+    window.getImage = this.getImage;
+  },
   methods: {
     //返回个人中心
     toSettings() {
@@ -55,19 +60,63 @@ export default {
     },
     //删除图片
     deleteImg(index) {
-      this.photo.splice(index,1);
+      this.photo.splice(index, 1);
+      this.showPhoto.splice(index, 1);
     },
-    //修改密码
+    //添加图片
+    addImg() {
+      this.downPictur("fedbackImg");
+    },
+    //获取图像
+    getImage(imgName, url) {
+      this.photo.push(imgName);
+      this.showPhoto.push(url);
+    },
+    //检查联系方式格式
+    checkPhoneNumber() {
+      if (this.phoneNumber) {
+        if (this.phoneNumber.length > 32) {
+          this.phoneNumberCheck = false;
+        } else {
+          this.phoneNumberCheck = validateContact(this.phoneNumber);
+        }
+      } else {
+        this.phoneNumberCheck = true;
+      }
+    },
+    //提交
     submit() {
+      if (this.problemContent.length === 0) {
+        MessageBox("提示", "反馈内容不能为空！");
+        return;
+      }
       MessageBox({
         title: "提示",
         message: "是否提交反馈？",
         showCancelButton: true
       }).then(action => {
         if (action == "confirm") {
-          MessageBox("提示", "感谢您的反馈！").then(action => {
-            this.$router.push("/settings");
-          });
+          if (this.phoneNumberCheck === false) {
+            MessageBox("提示", "联系方式输入有误，请检查");
+          } else {
+            this.$fetchPost(
+              "problemFeedback",
+              {
+                problemContent: this.problemContent,
+                photo: this.photo,
+                contact: this.phoneNumber
+              },
+              "json"
+            ).then(res => {
+              if (res.status === 0) {
+                MessageBox("提示", "感谢您的反馈！").then(action => {
+                  this.$router.push("/settings");
+                });
+              } else {
+                MessageBox("提示", "反馈失败");
+              }
+            });
+          }
         }
       });
     }
